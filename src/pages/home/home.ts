07830c4ca6bf74546d7694aa02d1aeae17a376e8
RayPetersB <rejon.peters@gmail.com>
2021-04-14 20:09:55 -0400

    commit on master

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -31,80 +31,124 @@ export class HomePage {
   constructor(public navCtrl: NavController, public toastController: ToastController,public alertController: AlertController) {
 
   }
-    removeItem(item, index) {
-      console.log("Removing Item - ", item, index);
-      const toast = this.toastController.create({
-        message: 'Removing Item - .' + index + "...",
-        duration: 3000
-      });
-      toast.present();
+  removeItem(item, index) {
+    console.log("Removing Item - ", item, index);
+    const toast = this.toastController.create({
+      message: 'Removing Item - .' + index + "...",
+      duration: 3000
+    });
+    toast.present();
 
-      this.items.splice(index,1) 
-
-
-    }
+    this.items.splice(index, 1)
 
 
-
-    editItem(item, index) {
-      console.log("Editing Item - ", item, index);
-      const toast = this.toastController.create({
-        message: 'Editing Item - .' + index + "...",
-        duration: 3000
-      });
-      toast.present();
-
-
-    }
-      addItem() {
-        console.log("Adding Item");
-        this.showAddItemPrompt();
-      }
-
-      showAddItemPrompt() {
-        let alert = this.alertController.create({
-          title: 'Add Item',
-          message: "Enter an Item",
-          inputs: [
-            {
-              name: 'name',
-              placeholder: 'Grocery Item Name'
-            },
-            {
-              name: 'quantity',
-              placeholder: 'Quantity',
-              
-            }
-          ],
-          buttons: [
-            {
-              text: 'Cancel',
-              role: 'cancel',
-              handler: data => {
-                console.log('Cancel clicked');
-              }
-            },
-            {
-              text: 'Save',
-              handler: item => {
-              console.log("Saved CLick",item);
-              this.items.push(item);
+  }
 
 
 
-                // if (item.isValid(item.name, item.quantity)) {
-                //   // logged in!
-                // } else {
-                //   // invalid login
-                //   return false;
-                
-              }
-            }
-          ]
-        });
-        alert.present();
-      }
+  editItem(item, index) {
+    console.log("Editing Item - ", item, index);
+    const toast = this.toastController.create({
+      message: 'Editing Item - .' + index + "...",
+      duration: 3000
+    });
+    toast.present();
+    this.showEditItemPrompt(item, index);
 
-   }
+
+  }
+  addItem() {
+    console.log("Adding Item");
+    this.showAddItemPrompt();
+  }
+
+  showAddItemPrompt() {
+    let alert = this.alertController.create({
+      title: 'Add Item',
+      message: "Enter an Item",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Grocery Item Name'
+        },
+        {
+          name: 'quantity',
+          placeholder: 'Quantity',
+
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: item => {
+            console.log("Saved CLick", item);
+            this.items.push(item);
+
+
+
+            // if (item.isValid(item.name, item.quantity)) {
+            //   // logged in!
+            // } else {
+            //   // invalid login
+            //   return false;
+
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+
+  showEditItemPrompt(item, index) {
+    let alert = this.alertController.create({
+      title: 'Edit Item',
+      message: "Please Item",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Grocery Item Name',
+          value: item.name
+        },
+        {
+          name: 'quantity',
+          placeholder: 'Quantity',
+          value: item.quantity
+
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: item => {
+            console.log("Saved CLick", item);
+            this.items[index] = item;
+
+
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+
+
+
+}
 
 
